8215b2186bbce97922bec84dd5d71eff89c2aa85
Patrick Schmidt <ps.patti1998@gmail.com>
2021-12-22 18:36:42 +0100

    Added install script and implemented send to fcm

--- mobileraker.py
+++ mobileraker.py
@@ -2,15 +2,17 @@
 
 # Press Umschalt+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import argparse
 import asyncio
 import json
 import logging
+import os
 import random
 from asyncio import AbstractEventLoop
 from copy import deepcopy
 
 import coloredlogs
+import requests
 import websockets
 from websockets.uri import WebSocketURI
 
@@ -20,13 +22,15 @@ from printer_objects import PrintStats, DisplayStatus, VirtualSDCard
 class Client:
     def __init__(
             self,
-            uri: WebSocketURI,
+            moonraker_uri: WebSocketURI,
+            fcm_uri: str,
             loop: AbstractEventLoop,
             apikey: str = None,
     ) -> None:
         super().__init__()
         self.websocket = None
-        self.server = uri
+        self.moonraker_server = moonraker_uri
+        self.mobileraker_fcm = fcm_uri
         self.apikey = apikey
         self.req_cb = {}
         self.req_blocking = {}
@@ -38,9 +42,10 @@ class Client:
         self.virtual_sdcard = VirtualSDCard()
         self.config = CompanionConfig()  # TODO: Fetch this from a remote server for easier configuration :)
         self.rec_task = None
+        self.logger = logging.getLogger('Client')
 
     async def connect(self) -> None:
-        async for websocket in websockets.connect(self.server):
+        async for websocket in websockets.connect(self.moonraker_server):
             try:
                 self.websocket = websocket
                 if self.rec_task:
@@ -59,7 +64,7 @@ class Client:
         response = json.loads(message)
         mid = response.get("id")
         if "error" in response and "message" in response["error"]:
-            self.websocket.logger.warning(
+            self.logger.warning(
                 "Error message received from WebSocket-Server %s" % response["error"]["message"])
             if mid and mid in self.req_cb:
                 await self.req_cb.pop(mid)(response, response["error"]["message"])
@@ -69,27 +74,28 @@ class Client:
             if mid and self.req_cb:
 
                 if mid in self.req_cb:
-                    self.websocket.logger.debug("Received a response to request: %d" % mid)
+                    self.logger.debug("Received a response to request: %d" % mid)
                     await self.req_cb.pop(mid)(response)
                 else:
-                    self.websocket.logger.error("Received a response to unknown request: %d" % mid)
+                    self.logger.error("Received a response to unknown request: %d" % mid)
                     self.req_cb.pop(mid)
             else:
-                self.websocket.logger.debug("Received a method notification for method: %s" % mmethod)
+                self.logger.debug("Received a method notification for method: %s" % mmethod)
                 if mmethod == "notify_status_update":
                     # "params": [{<status object>}, <eventtime>]
                     await self.parse_notify_status_update(response["params"][0])
                 elif mmethod == "notify_klippy_shutdown":
                     self.klippy_ready = False
                 else:
-                    self.websocket.logger.debug("Method %s not implemented/supported" % mmethod)
+                    self.logger.debug("Method %s not implemented/supported" % mmethod)
 
-    async def send_method(self, method: str, callback, params: dict = None) -> int:
+    async def send_method(self, method: str, callback=None, params: dict = None) -> int:
         req_dict = self.construct_json_rpc(method, params)
         message_json = json.dumps(req_dict)
-        self.req_cb[req_dict["id"]] = callback
+        if callback:
+            self.req_cb[req_dict["id"]] = callback
 
-        self.websocket.logger.info("Sending message %s" % message_json)
+        self.logger.debug("Sending message %s" % message_json)
         await self.websocket.send(message_json)
         return req_dict["id"]
 
@@ -102,7 +108,7 @@ class Client:
         self.req_cb[m_id] = self.receive_blocking_cb
         self.req_blocking[m_id] = response_future
 
-        self.websocket.logger.info("Sending message (Blocking) %s" % message_json)
+        self.logger.debug("Sending message (Blocking) %s" % message_json)
         await self.websocket.send(message_json)
         return await response_future
 
@@ -116,6 +122,7 @@ class Client:
         await self.parse_notify_status_update(message["result"]["status"])
 
     async def parse_notify_status_update(self, status_objects):
+        self.logger.debug("Received status update for %s" % status_objects)
         for key, object_data in status_objects.items():
             if key == "print_stats":
                 await self.parse_print_stats_update(object_data)
@@ -154,7 +161,7 @@ class Client:
             await self.on_print_state_transition(old.state, self.print_stats.state)
 
     async def parse_display_status_update(self, display_status):
-        incoming = DisplayStatus()
+        incoming = deepcopy(self.display_status)
 
         # Tbh. I dont care about the message
         if "message" in display_status:
@@ -167,11 +174,10 @@ class Client:
             self.send_to_firebase()
 
     async def parse_virtual_sdcard_update(self, virtual_sdcard):
-        incoming = VirtualSDCard()
+        incoming = deepcopy(self.virtual_sdcard)
 
-        # Tbh. I dont care about the message
         if "file_position" in virtual_sdcard:
-            incoming.progress = virtual_sdcard["file_position"]
+            incoming.file_position = virtual_sdcard["file_position"]
         if "progress" in virtual_sdcard:
             incoming.progress = virtual_sdcard["progress"]
 
@@ -191,7 +197,7 @@ class Client:
         await self.send_method("printer.objects.subscribe", self.parse_subscription_response, params)
 
     async def on_print_state_transition(self, old, new):
-        self.websocket.logger.info("print_state transition %s -> %s" % (old, new))
+        self.logger.info("print_state transition %s -> %s" % (old, new))
         self.send_to_firebase()
 
     def construct_json_rpc(self, method: str, params: dict = None) -> dict:
@@ -214,12 +220,14 @@ class Client:
         out = {
             "printState": print_state,
             "tokens": await self.fetch_fcm_tokens(),
-            "printerUUID": await self.fetch_printer_id()
+            "printerIdentifier": await self.fetch_printer_id()
         }
+
+        if self.print_stats and self.print_stats.filename:
+            out["filename"] = self.print_stats.filename
 
         if print_state == "printing":
             out["progress"] = self.virtual_sdcard.progress
-            out["filename"] = self.print_stats.filename
             out["printingDuration"] = self.print_stats.print_duration
         return out
 
@@ -232,10 +240,15 @@ class Client:
         # await self.send_method("server.database.get_item", self.fcm_token_received,
         #                        {"namespace": "mobileraker", "key": "fcmTokens"})
         msg = await self.collect_for_notification()
-        if msg["printerUUID"] is None:
-            self.websocket.logger.warning("Could not send to mobileraker-fcm, no printerUUID found!")
+        if msg["printerIdentifier"] is None:
+            self.logger.warning("Could not send to mobileraker-fcm, no printerIdentifier found!")
             return
-        print("SHOULD SEND TO FIREBASE: %s" % (json.dumps(msg)))
+        self.logger.info("Sending to firebase: %s" % json.dumps(msg))
+        try:
+            res = requests.post(self.mobileraker_fcm, json=msg)
+            await self.handle_fcm_send_response(res)
+        except requests.exceptions.ConnectionError as err:
+            self.logger.error("Could not reach the mobileraker server!")
 
     async def fetch_fcm_tokens(self):
         response, err = await self.send_and_receive_method("server.database.get_item",
@@ -252,6 +265,23 @@ class Client:
             return response["result"]["value"]
         return None
 
+    async def remove_fcm_token_at(self, index):
+        tokens = await self.fetch_fcm_tokens()
+        removed_token = tokens.pop(index)
+        self.logger.info("Trying to remove token %s" % removed_token)
+        await self.send_method("server.database.post_item", params=
+        {"namespace": "mobileraker", "key": "fcmTokens",
+         "value": tokens})
+
+    async def handle_fcm_send_response(self, res):
+        if res.status_code == 200:
+            message = res.content
+            response = json.loads(message)
+            responses = response['responses']
+            for idx, res in enumerate(responses):
+                if not res["successful"]:
+                    await self.remove_fcm_token_at(idx)
+
 
 class CompanionConfig:
 
@@ -262,9 +292,32 @@ class CompanionConfig:
 
 
 def main() -> None:
+    parser = argparse.ArgumentParser(
+        description="Mobileraker - Companion")
+    parser.add_argument(
+        "-l", "--logfile", default="/tmp/mobileraker.log", metavar='<logfile>',
+        help="log file name and location")
+    parser.add_argument(
+        "-n", "--nologfile", action='store_true',
+        help="disable logging to a file")
+
+    cmd_line_args = parser.parse_args()
+
+    if cmd_line_args.nologfile:
+        log_file = ""
+    elif cmd_line_args.logfile:
+        log_file = os.path.normpath(
+            os.path.expanduser(cmd_line_args.logfile))
+        fh = logging.FileHandler(log_file)
+        formatter = logging.Formatter(
+            '%(asctime)s [%(filename)s:%(funcName)s()] - %(message)s')
+        fh.setFormatter(formatter)
+        root_logger = logging.getLogger()
+        root_logger.addHandler(fh)
+
     event_loop = asyncio.get_event_loop()
     try:
-        client = Client(uri="ws://192.168.178.135/websocket", loop=event_loop)
+        client = Client(moonraker_uri='ws://127.0.0.1/websocket', fcm_uri='http://192.168.178.157:8080/companion/update', loop=event_loop)
         event_loop.create_task(client.connect())
         event_loop.run_forever()
     finally:
@@ -273,7 +326,6 @@ def main() -> None:
 
 
 coloredlogs.install(level=logging.INFO)
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
---
+++ scripts/install-mobileraker-companion.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+# This script installs the Mobileraker-Companion on a Raspi
+
+PYTHONDIR="${HOME}/mobileraker-env"
+SYSTEMDDIR="/etc/systemd/system"
+REBUILD_ENV="n"
+FORCE_DEFAULTS="n"
+LOG_PATH="/tmp/mobileraker.log"
+
+create_virtualenv()
+{
+    report_status "Installing python virtual environment..."
+
+    # If venv exists and user prompts a rebuild, then do so
+    if [ -d ${PYTHONDIR} ] && [ $REBUILD_ENV = "y" ]; then
+        report_status "Removing old virtualenv"
+        rm -rf ${PYTHONDIR}
+    fi
+
+    if [ ! -d ${PYTHONDIR} ]; then
+        virtualenv -p /usr/bin/python3 ${PYTHONDIR}
+    fi
+
+    # Install/update dependencies
+    ${PYTHONDIR}/bin/pip install -r ${SRCDIR}/scripts/mobileraker-requirements.txt
+}
+
+install_script()
+{
+# Create systemd service file
+    SERVICE_FILE="${SYSTEMDDIR}/mobileraker.service"
+    [ -f $SERVICE_FILE ] && [ $FORCE_DEFAULTS = "n" ] && return
+    report_status "Installing system start script..."
+    sudo /bin/sh -c "cat > ${SERVICE_FILE}" << EOF
+#Systemd service file for mobileraker
+[Unit]
+Description=Companion app to enable push notifications on mobileraker
+Requires=network-online.target
+After=network-online.target
+
+[Install]
+WantedBy=multi-user.target
+
+[Service]
+Type=simple
+User=$USER
+RemainAfterExit=yes
+WorkingDirectory=${SRCDIR}
+ExecStart=${LAUNCH_CMD} -l ${LOG_PATH}
+Restart=always
+RestartSec=10
+EOF
+# Use systemctl to enable the klipper systemd service script
+    sudo systemctl enable mobileraker.service
+    sudo systemctl daemon-reload
+}
+
+
+start_software()
+{
+    report_status "Launching mobileraker Companion..."
+    sudo systemctl restart mobileraker
+}
+
+# Helper functions
+report_status()
+{
+    echo -e "\n\n###### $1"
+}
+
+verify_ready()
+{
+    if [ "$EUID" -eq 0 ]; then
+        echo "This script must not run as root"
+        exit -1
+    fi
+}
+
+# Force script to exit if an error occurs
+set -e
+
+# Find SRCDIR from the pathname of this script
+SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
+LAUNCH_CMD="${PYTHONDIR}/bin/python ${SRCDIR}/mobileraker.py"
+
+# Parse command line arguments
+while getopts "rfc:l:" arg; do
+    case $arg in
+        r) REBUILD_ENV="y";;
+        f) FORCE_DEFAULTS="y";;
+        l) LOG_PATH=$OPTARG;;
+    esac
+done
+
+# Run installation steps defined above
+verify_ready
+create_virtualenv
+install_script
+start_software
