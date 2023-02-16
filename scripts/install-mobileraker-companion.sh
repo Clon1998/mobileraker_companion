c7af7783f62139fd2da7b85dc343b8e66a027d60
Patrick Schmidt <patrick.schmidt.25@stud.tu-darmstadt.de>
2023-02-16 13:38:49 +0100

    Update install-mobileraker-companion.sh

--- scripts/install-mobileraker-companion.sh
+++ scripts/install-mobileraker-companion.sh
@@ -36,8 +36,7 @@ install_script()
 #Systemd service file for mobileraker
 [Unit]
 Description=Companion app to enable push notifications on mobileraker
-Requires=network-online.target
-After=network-online.target
+After=network-online.target moonraker.service
 
 [Install]
 WantedBy=multi-user.target
@@ -45,7 +44,6 @@ WantedBy=multi-user.target
 [Service]
 Type=simple
 User=$USER
-RemainAfterExit=yes
 WorkingDirectory=${SRCDIR}
 ExecStart=${LAUNCH_CMD} -l ${LOG_PATH}
 Restart=always
