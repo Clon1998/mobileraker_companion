06a58b5f5c4ed8d7c52b71456e900ed6c9b628b2
Patrick Schmidt <ps.patti1998@gmail.com>
2023-02-27 17:28:46 +0100

    #20 - Refactored config file location checks Refactored how config file locatioations are checked and added the new printer_data/config one

--- configs.py
+++ configs.py
@@ -4,11 +4,16 @@ import logging
 import os
 import pathlib
 import time
-from typing import Any, Dict
+from typing import Any, Dict, Optional, Union
 
 import pytz
 
-script_dir = pathlib.Path(__file__).parent.resolve().parent
+user_dir = os.path.expanduser("~/")
+companion_dir = pathlib.Path(__file__).parent.resolve()
+klipper_config_dir = os.path.join(
+    user_dir, "klipper_config")
+printer_data_config_dir = os.path.join(
+    user_dir, "printer_data", "config")
 
 
 class CompanionRemoteConfig:
@@ -23,11 +28,15 @@ class CompanionRemoteConfig:
 class CompanionLocalConfig:
     default_file_name: str = "Mobileraker.conf"
 
-    def __init__(self, configfile: str) -> None:
+    def __init__(self, passed_config_file: str) -> None:
         super().__init__()
         self.config: configparser.ConfigParser = configparser.ConfigParser()
         self.printers: Dict[str, Dict[str, Any]] = {}
-        self.config.read(self.get_config_file_location(configfile))
+
+        config_file_path = self.get_config_file_location(passed_config_file)
+        if config_file_path:
+            self.config.read(config_file_path)
+
         printer_sections = sorted(
             [i for i in self.config.sections() if i.startswith("printer ")])
 
@@ -57,25 +66,47 @@ class CompanionLocalConfig:
         logging.info(
             f'Main section read, language:"{self.language}", timezone:"{self.timezone_str}", eta_format:"{self.eta_format}"')
 
-    def get_config_file_location(self, file: str) -> str:
-        logging.info("Passed config file is: %s" % file)
-        if not os.path.exists(file):
-            file = os.path.join(script_dir, self.default_file_name)
-            if not os.path.exists(file):
-                file = self.default_file_name.lower()
-                if not os.path.exists(file):
-                    klipper_config = os.path.join(
-                        os.path.expanduser("~/"), "klipper_config")
-                    file = os.path.join(klipper_config, self.default_file_name)
-                    if not os.path.exists(file):
-                        file = os.path.join(
-                            klipper_config, self.default_file_name.lower())
-                        # if not os.path.exists(file):
-                        #     file = self.default_config_path
-        if os.path.exists(file):
+    def get_config_file_location(self, passed_config: str) -> Optional[str]:
+        logging.info("Passed config file is: %s" % passed_config)
+
+        foundFile = passed_config if os.path.exists(passed_config) else self.__check_companion_dir() or self.__check_klipper_config_dir(
+            ) or self.__check_printer_data_config_dir() or self.__check_user_dir()
+
+
+        if foundFile and os.path.exists(foundFile):
             logging.info("Found configuration file at: %s" %
-                         os.path.abspath(file))
+                         os.path.abspath(foundFile))
         else:
             logging.warn(
                 "No config file was found, using default fallback values!")
-        return file
+        return foundFile
+
+    # Check if file exists, ignoring if it starts capatilized or lowercase!
+    def __check_file_exists(self, path: Union[pathlib.Path, str], filename: str) -> Optional[str]:
+        file = os.path.join(path, filename)
+        if os.path.exists(file):
+            return file
+        # Also check lower case config name!
+        file = os.path.join(path, filename.lower())
+        if os.path.exists(file):
+            return file
+
+    # Checks the companion dir
+    def __check_companion_dir(self) -> Optional[str]:
+        logging.info("Checking mobileraker_companion dir")
+        return self.__check_file_exists(companion_dir, self.default_file_name)
+
+    # Checks deprecated klipper_config dir
+    def __check_klipper_config_dir(self) -> Optional[str]:
+        logging.info("Checking klipper_config dir")
+        return self.__check_file_exists(klipper_config_dir, self.default_file_name)
+
+    # Check new ~/printer_data/config
+    def __check_printer_data_config_dir(self) -> Optional[str]:
+        logging.info("Checking printer_data/config dir")
+        return self.__check_file_exists(printer_data_config_dir, self.default_file_name)
+
+    # Check user-dir -> ~/Mobileraker.conf
+    def __check_user_dir(self) -> Optional[str]:
+        logging.info("Checking user dir")
+        return self.__check_file_exists(user_dir, self.default_file_name)
