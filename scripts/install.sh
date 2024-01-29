eaf0d5d92906453adb0695b9f5bc81c8f67a9b68
Patrick Schmidt <ps.patti1998@gmail.com>
2024-01-29 16:16:50 +0100

    Fix for fix..

--- scripts/install.sh
+++ scripts/install.sh
@@ -315,11 +315,8 @@ then
     install_or_update_system_dependencies
     # Now make sure the virtual env exists, is updated, and all of our currently required PY packages are updated.
     install_or_update_python_env
-<<<<<<< HEAD
-=======
-fi
->>>>>>> 3ef8e96 (Fixed if)
 
+fi
 # Before launching our PY script, set any vars it needs to know
 # Pass all of the command line args, so they can be handled by the PY script.
 # Note that USER can be empty string on some systems when running as root. This is fixed in the PY installer.
