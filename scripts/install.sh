13576536340835044528a04d8b7a8c062eab6250
Cyril Guislain <guilouz6@hotmail.com>
2024-05-14 11:19:54 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -166,7 +166,11 @@ ensure_py_venv()
     mkdir -p "${ENV_DIR}"
     if [ "$IS_K1_OS" -eq 1 ]; then
         # The K1 requires we setup the virtualenv like this.
-        virtualenv -p /opt/bin/python3 --system-site-packages "${ENV_DIR}"
+        if [[ -f /opt/bin/python3 ]]; then
+            virtualenv -p /opt/bin/python3 --system-site-packages "${ENV_DIR}"
+        else
+            python3 /usr/lib/python3.8/site-packages/virtualenv.py -p /usr/bin/python3 --system-site-packages "${ENV_DIR}"
+        fi
     else
         # Everything else can use this more modern style command.
         virtualenv -p /usr/bin/python3 --system-site-packages "${ENV_DIR}"
@@ -185,7 +189,9 @@ install_or_update_system_dependencies()
         # the user might install opkg via the 3rd party moonraker installer script.
         # But in general, PY will already be installed, so there's no need to try.
         # On the K1, the only we thing we ensure is that virtualenv is installed via pip.
-        opkg install ${CREALITY_DEP_LIST}
+        if [[ -f /opt/bin/opkg ]]; then
+            opkg install ${CREALITY_DEP_LIST}
+        fi
         pip3 install --trusted-host pypi.python.org --trusted-host pypi.org --trusted-host=files.pythonhosted.org --no-cache-dir virtualenv
     elif [ "$IS_SONIC_PAD_OS" -eq 1 ]; then
         # The sonic pad always has opkg installed, so we can make sure these packages are installed.
