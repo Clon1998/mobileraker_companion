fe8dcb764b5a51c189cb99f4ccb7208f743f54a2
Patrick Schmidt <ps.patti1998@gmail.com>
2024-09-07 15:37:22 +0200

    dtos: Added str method to ServerInfo

--- mobileraker/data/dtos/moonraker/printer_objects.py
+++ mobileraker/data/dtos/moonraker/printer_objects.py
@@ -19,6 +19,12 @@ class ServerInfo:
         if "result" in json:
             n.message = json["result"]
         return n
+    
+    def __str__(self):
+        return '%s(%s)' % (
+            type(self).__name__,
+            ', '.join('%s=%s' % item for item in vars(self).items())
+        )
 
 
 class PrintStats:
